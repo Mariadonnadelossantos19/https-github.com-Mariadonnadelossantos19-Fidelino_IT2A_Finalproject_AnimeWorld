6c8ffefb918abad83ccf6cfc6323bccc33ed7bce
Mariadonnadelossantos19 <mariadelossantos207@gmail.com>
2023-05-27 15:06:19 +0800

    My thirteenth Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Login"?></title>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="script/login1.css">
+</head>
+
+<body>
+    <div class="container">
+        
+        <div class="design">
+            <div class="pill-1 rotate-45"></div>
+            <div class="pill-2 rotate-45"></div>
+            <div class="pill-3 rotate-45"></div>
+            <div class="pill-4 rotate-45"></div>
+        </div>
+        <div class="login">
+            
+
+            <a href="login.php"><button type="submit" class="login-btn", style="color:black ; width: 150px;"><?php echo "Explore"?></button></a>
+            
+        </div>
+    </div>
+</body>
+
+</html>
